423b901ed5f1352d6d02cb07871cbe93ef2be3b4
Sarathmon K R <sarathmonkr1234@gmail.com>
2025-02-26 20:44:52 +0530

    Refactor Header component title for improved styling and hover effects

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -5,7 +5,10 @@ const Header = () => {
         <header className="bg-gray-900 text-white p-4 shadow-md transition duration-300 ease-in-out">
             <div className="container mx-auto flex justify-between items-center">
                 <Link to={'/'}>
-                    <h1 className="text-2xl font-bold hover:text-gray-400 transition duration-300 ease-in-out cursor-pointer">Go Cinema</h1></Link>
+                    <h1 className="text-2xl font-bold transition duration-300 ease-in-out cursor-pointer">
+                        <span className="hover:text-gray-400 text-blue-500">Go</span> <span className="hover:text-gray-400 text-white-500">Cinema</span>
+                    </h1>
+                </Link>
             </div>
         </header>
     );
